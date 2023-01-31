d4b77d3218bbf3c16e920f54af21587205949763
JellyKingdom <jyhan1993@gmail.com>
2023-01-31 20:40:23 +0900

    feat: add two more titles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 function App() {
     let post = "강남 우동 맛집";
-    let [Blog_title, b] = useState("temp_title");
+    let [Blog_title, b] = useState(['첫번째 제목입니다.','두번째 제목입니다.','세번째도 잘 들어가나요?']);
 
     return (
         <div className="App">
@@ -12,7 +12,15 @@ function App() {
                 <h4>블로그임</h4>
             </div>
             <div className="list">
-                <h4>{Blog_title}</h4>
+                <h4>{Blog_title[0]}</h4>
+                <p>1월 31일 발행</p>
+            </div>
+            <div className="list">
+                <h4>{Blog_title[1]}</h4>
+                <p>1월 31일 발행</p>
+            </div>
+            <div className="list">
+                <h4>{Blog_title[2]}</h4>
                 <p>1월 31일 발행</p>
             </div>
         </div>
