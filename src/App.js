fb67a15b44a5eedfe0b2dead043c5c2965fa0f51
JellyKingdom <jyhan1993@gmail.com>
2023-01-31 20:28:02 +0900

    feat: add title with useState

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import { useState } from "react";
 
 let post = "강남 우동 맛집";
-let [a,b] = useState('temp_title');
+let [title,b] = useState('temp_title');
 
 function App() {
     return (
@@ -12,7 +12,7 @@ function App() {
                 <h4>블로그임</h4>
             </div>
             <div className="list">
-              <h4>제목입니다.</h4>
+              <h4>{title}</h4>
               <p>1월 31일 발행</p>
             </div>
         </div>
