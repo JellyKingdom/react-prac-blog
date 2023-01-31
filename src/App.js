b2c61fbb12fd0dac3f8eaa573ff49d410e5c6460
JellyKingdom <jyhan1993@gmail.com>
2023-01-31 20:24:05 +0900

    feat: title UI

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import logo from "./logo.svg";
 import "./App.css";
 
 let post = "강남 우동 맛집";
+
 function App() {
     return (
         <div className="App">
             <div className="black-nav">
                 <h4>블로그임</h4>
             </div>
-            <div style={{ color: "red", fontSize: "16px" }}>{post}</div>
+            <div className="list">
+              <h4>제목입니다.</h4>
+              <p>1월 31일 발행</p>
+            </div>
         </div>
     );
 }
