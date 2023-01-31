48ca6a03c36c2a8db07c5aa709cbfd1ff0405444
JellyKingdom <jyhan1993@gmail.com>
2023-01-31 20:07:13 +0900

    feat: add style

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-let post = '강남 우동 맛집';
+let post = "강남 우동 맛집";
 function App() {
-  return (
-    <div className="App">
-      <div className="black-nav">
-        <h4>블로그임</h4>
-        <div>{ post }</div>
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="black-nav">
+                <h4>블로그임</h4>
+            </div>
+            <div style={{ color: "red", fontSize: "16px" }}>{post}</div>
+        </div>
+    );
 }
 
 export default App;
