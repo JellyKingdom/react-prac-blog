a620ef1313207beecafa635394e7aff0713ccd02
JellyKingdom <jyhan1993@gmail.com>
2023-01-31 20:37:16 +0900

    fix: react error and change state name

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
-let post = "강남 우동 맛집";
-let [title,b] = useState('temp_title');
-
 function App() {
+    let post = "강남 우동 맛집";
+    let [Blog_title, b] = useState("temp_title");
+
     return (
         <div className="App">
             <div className="black-nav">
                 <h4>블로그임</h4>
             </div>
             <div className="list">
-              <h4>{title}</h4>
-              <p>1월 31일 발행</p>
+                <h4>{Blog_title}</h4>
+                <p>1월 31일 발행</p>
             </div>
         </div>
     );
