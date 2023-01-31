ce135da937cbead8984b8caf8db1c77c26df4eb0
JellyKingdom <jyhan1993@gmail.com>
2023-01-31 20:05:28 +0900

    feat: add blog title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+let post = '강남 우동 맛집';
 function App() {
   return (
     <div className="App">
       <div className="black-nav">
         <h4>블로그임</h4>
+        <div>{ post }</div>
       </div>
     </div>
   );
